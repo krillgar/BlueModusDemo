3cfcf949a3a9a7362e5259a16c143100b1d7a76e
Ben Sheppard <krillgar@gmail.com>
2025-01-12 15:23:14 -0500

    Created RedirectionService and added logging (#2)
    Created a RedirectionService and added logging.

---
+++ BlueModusDemo.Services/IRedirectionService.cs
@@ -0,0 +1,13 @@
+﻿namespace BlueModusDemo.Services;
+
+/// <summary>
+/// An interface for retrieving redirection settings.
+/// </summary>
+internal interface IRedirectionService
+{
+    /// <summary>
+    /// A method to retrieve the current redirection settings.
+    /// </summary>
+    /// <returns></returns>
+    Task<string> GetRedirections();
+}
---
+++ BlueModusDemo.Services/RedirectionService.cs
@@ -0,0 +1,39 @@
+﻿using BlueModusDemo.Services.Models;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace BlueModusDemo.Services;
+
+/// <summary>
+/// A class to retrieve the redirection URLs for the demo application.
+/// </summary>
+public class RedirectionService : IRedirectionService
+{
+    private readonly IReadOnlyCollection<RedirectResult> _redirectResults = new[] {
+        new RedirectResult() {
+            RedirectUrl = "/campaignA",
+            TargetUrl = "/campaigns/targetcampaign",
+            RedirectType = HttpStatusCode.Found,
+            UseRelative = false
+        },
+        new RedirectResult()
+        {
+            RedirectUrl = "/campaignB",
+            TargetUrl = "/campaigns/targetcampaign/channelB",
+            RedirectType = HttpStatusCode.Found,
+            UseRelative = false
+        },
+        new RedirectResult() {
+            RedirectUrl = "/product-directory",
+            TargetUrl = "/products",
+            RedirectType = HttpStatusCode.MovedPermanently,
+            UseRelative = true
+        }
+    };
+
+    /// <inheritdoc />
+    public Task<string> GetRedirections()
+    {
+        return Task.FromResult(JsonConvert.SerializeObject(_redirectResults));
+    }
+}
