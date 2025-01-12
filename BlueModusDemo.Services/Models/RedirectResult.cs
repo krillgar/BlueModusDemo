083dbbba2e87a25d1497737a8439e2b68d9d721b
Ben Sheppard <krillgar@gmail.com>
2025-01-12 15:20:54 -0500

    Created RedirectionService and added logging

---
+++ BlueModusDemo.Services/Models/RedirectResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace BlueModusDemo.Services.Models;
+
+public record RedirectResult
+{
+    public string RedirectUrl { get; set; } = string.Empty;
+    public string TargetUrl { get; set; } = string.Empty;
+    public HttpStatusCode RedirectType { get; set; } = HttpStatusCode.OK;
+    public bool UseRelative { get; set; } = false;
+}
--- BlueModusDemo/Program.cs
+++ BlueModusDemo/Program.cs
@@ -1,11 +1,16 @@
+using BlueModusDemo.Services.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services
+    .AddLogging()
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .UseServices();
 
 var app = builder.Build();
 
