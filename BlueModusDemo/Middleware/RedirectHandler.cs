a909af779d081b047fa88088222c11c0b0b2bfe6
Ben Sheppard <krillgar@gmail.com>
2025-01-13 14:39:40 -0500

    Created Middleware to handle redirections (#3)
    Created Middleware to handle redirections.
    Fixed some minor visibility with last PR.

---
+++ BlueModusDemo/Middleware/RedirectHandler.cs
@@ -0,0 +1,99 @@
+﻿using BlueModusDemo.Services;
+using BlueModusDemo.Services.Models;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace BlueModusDemo.Middleware;
+
+public class RedirectHandler : DelegatingHandler
+{
+    private readonly ILogger _logger;
+    private static IEnumerable<RedirectResult> _routes = Enumerable.Empty<RedirectResult>();
+    private readonly static object _lock = new object();
+
+    public RedirectHandler(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory?.CreateLogger<RedirectHandler>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+    }
+
+    public async Task<HttpResponseMessage> ProcessAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        return await SendAsync(request, cancellationToken);
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var originalPath = request.RequestUri;
+        var (path, statusCode) = GetRedirectPath(originalPath?.LocalPath ?? string.Empty);
+
+        try
+        {
+            HttpResponseMessage response;
+
+            if (statusCode == HttpStatusCode.OK)
+            {
+                response = await base.SendAsync(request, cancellationToken);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Successful call to {path}", path);
+                }
+                else
+                {
+                    _logger.LogError("Error calling {path}: {reason}", path, response.ReasonPhrase);
+                }
+            }
+            else
+            {
+                path += originalPath?.Query;
+                response = new HttpResponseMessage(statusCode);
+
+                response.Headers.Add(HeaderNames.Location, path);
+
+                _logger.LogInformation("{statusCode} redirection from {old} to {new}.", statusCode, originalPath, path);
+            }
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error trying to execute {path}: {message}", path, ex.Message);
+
+            return new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.InternalServerError
+            };
+        }
+    }
+
+    private (string, HttpStatusCode) GetRedirectPath(string path)
+    {
+        var redirect = _routes.FirstOrDefault(route => path.Contains(route.RedirectUrl, StringComparison.InvariantCultureIgnoreCase));
+        var statusCode = HttpStatusCode.OK;
+
+        if (redirect != null)
+        {
+            statusCode = redirect.RedirectType;
+            path = redirect.UseRelative ?
+                path.Replace(redirect.TargetUrl, redirect.RedirectUrl) :
+                redirect.RedirectUrl;
+        }
+
+        return (path, statusCode);
+    }
+
+    public static async Task PopulateRoutes(IRedirectionService service, ILogger logger)
+    {
+        var results = await service.GetRedirectionsAsync();
+
+        var routes = JsonConvert.DeserializeObject<IEnumerable<RedirectResult>>(results) ?? Enumerable.Empty<RedirectResult>();
+
+        lock (_lock)
+        {
+            _routes = routes;
+        }
+
+        logger.LogInformation("Routes repopulated at {now}.", DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss.fff"));
+    }
+}
---
+++ BlueModusDemo/Middleware/RedirectMiddleware.cs
@@ -0,0 +1,37 @@
+﻿namespace BlueModusDemo.Middleware;
+
+public class RedirectMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly RedirectHandler _handler;
+
+    public RedirectMiddleware(RequestDelegate next, RedirectHandler handler)
+    {
+        _next = next ?? throw new ArgumentNullException(nameof(next));
+        _handler = handler ?? throw new ArgumentNullException(nameof(handler));
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var request = new HttpRequestMessage();
+        request.RequestUri = new Uri($"{context.Request.Scheme}://{context.Request.Host}{context.Request.Path}{context.Request.QueryString}");
+        request.Method = new HttpMethod(context.Request.Method);
+
+        var response = await _handler.ProcessAsync(request, CancellationToken.None);
+
+        context.Response.StatusCode = (int)response.StatusCode;
+
+        foreach (var header in response.Headers)
+        {
+            context.Response.Headers[header.Key] = header.Value.ToArray();
+        }
+
+        foreach (var header in response.Content.Headers)
+        {
+            context.Response.Headers[header.Key] = header.Value.ToArray();
+        }
+
+        var content = await response.Content.ReadAsByteArrayAsync();
+        await context.Response.Body.WriteAsync(content, 0, content.Length);
+    }
+}
